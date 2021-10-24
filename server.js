c7567bd17d82de57b2c1e9736ec23244f0e0b2e0
Zac Goad <zjgoad@gmail.com>
2021-10-24 15:55:58 -0400

    server.js update with adding capabilities for departments and salaries

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const inquirer = require('inquirer');
 const connect = mysql.createConnection({
     host: 'localhost',
@@ -9,10 +9,19 @@ const connect = mysql.createConnection({
 
 // Start the process
 const init = () => {
-    console.log('Welcome to the Employee Database!')
+    console.log(`
+    -- Welcome to our Employee Database! Please select from the following options... --
+    `)
     questionList();
 }
 
+// End the process
+const quitProgram = () => {
+    console.log(`
+    -- Thank you for using our Employee Database! --
+    `);
+    process.exit(0);
+}
 const questionList = () => {
     inquirer.prompt([
         {
@@ -30,10 +39,8 @@ const questionList = () => {
             ]
         }
     ])
-
 // Depending on the user's choice, a different function will execute.  A switch/case seemed like the
 // effective way to deal with this.
-
 .then ((answer) =>{
     switch(answer.option) {
         case 'View Departments':
@@ -60,9 +67,137 @@ const questionList = () => {
         }
     })
 };
+const viewDepartments = () => {
+    db.query(`SELECT id, department.name AS Name FROM department`, (err, department) => {
+        if (err){
+            console.error(err);
+        } else {
+            console.log(`
+            -- Departments --
+            `);
+            console.table(department);
+            questionList();
+        }
+    })
+}
+const viewPositions = () => {
+    db.query(`SELECT positions.id
+              positions.title AS title,
+              positions.salary AS salary,
+              positions.department_id AS department,
+              FROM positions
+              LEFT JOIN department ON positions.department_id = department.id
+              `, (err, res) => {
+                  if (err) {
+                      console.error(err);
+                  } else {
+                      console.log(`
+                      -- Positions --
+                      `); 
+                      console.log(res);
+                      questionList();
+        }
+    })
+}
+// View the employees and their information
+const viewEmployees = () => {
+    db.query(`
+    SELECT employees.id,
+           employees.first_name AS firstName,
+           employees.last_name AS lastName,
+           employees.positions_id AS title,
+           positions.salary AS salary,
+           department.name AS department,
+           FROM employees
+           LEFT JOIN department ON positions.department_id = department.id,
+           LEFT JOIN manager ON manager.id = employees.manager_id,
+           ORDER BY employees.id
+           `, (err, res) => {
+               if (err) {
+                   console.error(err);
+               } else {
+                   console.log(`
+                   -- Employees --
+                   `);
+                   console.table(res);
+                   questionList();
+        }
+    })
+}
+// Add another department to the database
+const createDepartment = () => {
+    console.log(`
+    -- Add Department --
+    `);
 
+    db.query(`SELECT * FROM department`, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.table(res);
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'departmentName',
+                    message: 'What is your new department\'s name?'
+                }
+            ])
+        }
+    })
+    // a promise to check for an error if one pops up
+    .then((answer) => {
+        db.query(`INSERT INTO department (name)VALUES (?)`, [answer.department]), (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`
+                -- ${answer.department} successfully added to the database --
+                `);
+                questionList();
+            }
+        }
+    })
+}
+// Ad another position to the database
+const creatPosition = () => {
+    console.log(`
+    -- Add Position --
+    `);
 
-
+    db.query(`SELECT * FROM department`, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const departmentChoices = department.map(({ name, id }) => ({name: name, value: id}));
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'positionName',
+                    message: 'What is your new position\'s name?',
+                    validate: function(isValid) {
+                        if(isValid === ''){
+                            return 'Please enter a valid response';
+                        } else {
+                            return true;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'salaryAmount',
+                    message: 'What is the new position\'s salary?',
+                    validate: function(isNumber) {
+                        if(isNAN(isNumber) || Number === ''){
+                            return 'Please enter a valid response';
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+            ])
+        }
+    })
+}
 
 
 
